4186a21b5e437d07d9f1c1d14fc52bf278f8338d
Bruno Alfred <alfredybruno@gmail.com>
2020-04-14 17:19:49 +0300

    middleware lesson

--- rest/app.js
+++ rest/app.js
@@ -5,4 +5,9 @@ const port = 5000;
 // ? Listening to a port
 app.listen(port, () => {
     console.log(`Now Listeninf to port ${port}`)
-})
+});
+
+app.use((req, res, next) => {
+    console.log('Time:', Date.now());
+    next();
+});
