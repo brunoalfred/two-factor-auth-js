27efaef8668d13be59dc8dd5af1db1ad044f3bab
Bruno Alfred <alfredybruno@gmail.com>
2020-04-14 14:16:14 +0300

    created a middleware

--- rest/app.js
+++ rest/app.js
@@ -5,6 +5,12 @@ const app = express();
 // ? Listening to Port a server at port 5000
 app.listen(5000);
 
+// ? MiddleWare
+app.use('/posts', ()=> {
+    console.log('This is a MiddleWare Running');
+});
+
+
 
 // ? Routes 
 app.get('/', (req, res) => {
