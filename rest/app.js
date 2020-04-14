78873a468625d32979f557a3595ead880dfa07a3
Bruno Alfred <alfredybruno@gmail.com>
2020-04-14 17:17:17 +0300

    creating app listening to port 5000

--- rest/app.js
+++ rest/app.js
@@ -2,33 +2,7 @@ const express = require('express');
 const app = express();
 const port = 5000;
 
-// ? Listening to a port 5000 in a server
+// ? Listening to a port
 app.listen(port, () => {
-    console.log(`Listening to server in port ${port}`);
-});
-
-
-// ! Creating a Route
-
-// ? A Three callBacks
-
-var cb0 = function(req, res, next) {
-    console.log('First CallBack');
-    next();
-}
-
-var cb1 = function(req, res, next) {
-    console.log('Second CallBack');
-    next();
-}
-
-var cb2 = function(req, res, next) {
-    res.send('I\'m the CallBack 3');
-    next();
-}
-
-var cb3 = function(req, res) {
-    console.log("The final CallBack");
-}
-
-app.get('/', [cb0, cb1, cb2, cb3]);
+    console.log(`Now Listeninf to port ${port}`)
+})
