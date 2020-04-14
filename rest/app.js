4ce26079cbc4302598a497ca11f3b63573d6c253
Bruno Alfred <alfredybruno@gmail.com>
2020-04-14 10:45:11 +0300

    making another response

--- rest/app.js
+++ rest/app.js
@@ -1,12 +1,18 @@
 const express = require('express');
 
-app.express();
-
-// ? Routes
-app.get('/', (req, res) => {res.send('We are on the Home Folder!')});
-
-
+const app = express();
 
 // ? Listening to Port a server at port 5000
 app.listen(5000);
+
+
+// ? Routes
+app.get('/', (req, res) => {
+    res.send('We are on the Home Folder!');
+});
+
+app.get('/posts' (req, res) => {res.send()});
+
+
+
 
