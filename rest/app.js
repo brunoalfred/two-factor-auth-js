da81893c5ed57ea0bc82a2c6883e371cab3ff5af
Bruno Alfred <alfredybruno@gmail.com>
2020-04-14 15:45:46 +0300

    listening to a port

--- rest/app.js
+++ rest/app.js
@@ -1,17 +1,8 @@
-
 const express = require('express');
 const app = express();
 const port = 5000;
 
-// ? LIstening to a port in a server
-app.listen(port, () =>
-    console.log(`Now listening at server in port ${port}`));
-
-
-//  Route created
-
-// ?  For a get Request
-app.get('/', (req, res) => {
-    res.send('Hello World');
+// ? Listening to a port 5000 in a server
+app.listen(port, () => {
+    console.log(`Listening to server in port ${port}`);
 });
-
