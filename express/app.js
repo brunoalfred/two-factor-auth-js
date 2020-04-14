ad642d42c4d9f17421a94ee2a3842b96869052b7
Bruno Alfred <alfredybruno@gmail.com>
2020-04-15 02:49:44 +0300

    syntax

--- express/app.js
+++ express/app.js
@@ -4,9 +4,12 @@ const app = express();
 const port = process.env.port || 5000;
 
 // ? Listening to a port where a server was Sta
-app.listen(port, () => {
-    console.log(`Now Listening at port ${port}`);
-});
+app.listen(port, () => console.log(`Now Listening at port ${port}`)
+);
 
 // ? creating an endpoint to a home page
 app.use(express.static(path.join(__dirname, 'public')));
+
+
+// * FOr sample get request
+app.get('/api/member')
