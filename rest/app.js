ae6900419971017d643893fc1d4996be05d49771
Bruno Alfred <alfredybruno@gmail.com>
2020-04-14 15:10:19 +0300

    deletion of the content

--- rest/app.js
+++ rest/app.js
@@ -15,17 +15,3 @@ app.get('/', (req, res) => {
     res.send('Hello World');
 });
 
-// ? For a post Request
-app.post('/sent', (req, res) => {
-    res.send('Post Method Successful');
-});
-
-// ? For a PUT method
-app.put('/put', (req, res) => {
-    res.send('Put Method Succesfull');
-});
-
-// ? For a DELETE MEthod
-app.delete('/deletion', (req, res) => {
-    res.send('Deletion Method Successful');
-})
