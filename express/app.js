9b6455a0c4cc75729e9f5e8805e24591d9c3b92d
Bruno Alfred <alfredybruno@gmail.com>
2020-04-15 02:22:34 +0300

    static folder

--- express/app.js
+++ express/app.js
@@ -9,7 +9,4 @@ app.listen(port, () => {
 });
 
 // ? creating an endpoint to a home page
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-    
-});
+app.use(express.static(path.join(__dirname, 'public')));
