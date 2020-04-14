a9eb55e3a32483921ab23870d3a2ae0acdf197c6
Bruno Alfred <alfredybruno@gmail.com>
2020-04-15 00:08:19 +0300

    middle ware

--- rest/app.js
+++ rest/app.js
@@ -5,15 +5,20 @@ const port = 5000;
 // ? Listening to a port
 app.listen(port, () => {
     console.log(`Now Listening to port ${port}`);
-    
+
 });
 
 
 // ? MiddleWare functions
-
+app.use('/', () => {
+    console.log('THe Middle Ware is running');
+});
 
 
 
 
 
 // ? ROutes
+app.get('/', (req, res) => {
+    res.send('Home Page')
+});
