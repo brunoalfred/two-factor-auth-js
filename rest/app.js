610874f3021325d2d0bdd181581a17aaf43f28f1
Bruno Alfred <alfredybruno@gmail.com>
2020-04-14 17:33:17 +0300

    gave a GET response on the console

--- rest/app.js
+++ rest/app.js
@@ -4,10 +4,12 @@ const port = 5000;
 
 // ? Listening to a port
 app.listen(port, () => {
-    console.log(`Now Listeninf to port ${port}`)
+    console.log(`Now Listening to port ${port}`)
 });
 
-app.use((req, res, next) => {
-    console.log('Time:', Date.now());
+
+// ? MiddleWare functions
+app.use('/user/:id', (req, res, next) => {
+    console.log('Request Type:', req.method);
     next();
 });
