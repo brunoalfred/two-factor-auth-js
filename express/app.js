de8efa302028ac5f4b5b2db671cad1698b6ddc1e
Bruno Alfred <alfredybruno@gmail.com>
2020-04-15 15:34:03 +0300

    ewgbtehbreth

--- express/app.js
+++ express/app.js
@@ -6,12 +6,13 @@ const members = require('./Members');
 const port = process.env.port || 5000;
 
 // ? Listening to a port where a server was Sta
-app.listen(port, () => console.log(`Now Listening at port ${port}`)
-);
+app.listen(port, () =>
+    console.log(`Now Listening at port ${port}`));
 
 // ? creating an endpoint to a home page
-// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 
-// * FOr sample get request
-app.get('/member', (req, res, next)=> res.json(members));
+// * FOr sample get requestc for all members
+
+app.get('/member', (req, res, next) => res.json(members));
