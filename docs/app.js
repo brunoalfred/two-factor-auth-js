d1452223e5beead3f3974908331af7774f542b1a
Bruno Alfred <alfredybruno@gmail.com>
2020-04-17 16:28:55 +0300

    custome modules importing

---
+++ docs/app.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const app = express();
+const port = process.env.port || 5000;
+
+//import custome modules
--- docs/birds.js
+++ docs/birds.js
@@ -2,10 +2,13 @@ const express = require('express');
 const router = express.Router();
 
 // middleware to log time
-router.use(function (req, res) {
+router.use(function (req, res, next) {
     console.log('Time:', Date.now())    ;
     next();
 
 });
 // homepage route
-router.get('/', () => res.send('This is Home Page'));
+router.get('/', (req, res) => res.send('This is Home Page'));
+
+// About route
+router.get('/about', (req, res) => res.send('This is About Page'));
