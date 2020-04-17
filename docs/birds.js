43a819dcb36f53bbc511a169f288c4ee05880c29
Bruno Alfred <alfredybruno@gmail.com>
2020-04-17 16:20:16 +0300

    implemented a middleware

--- docs/birds.js
+++ docs/birds.js
@@ -1,2 +1,9 @@
 const express = require('express');
 const router = express.Router();
+
+// middleware to log time
+router.use(function (req, res) {
+    console.log('Time:', Date.now())    ;
+    next();
+
+})
