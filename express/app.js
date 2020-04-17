fe18b76b4ac6a9bcaddb21574612d06c987f5aeb
Bruno Alfred <alfredybruno@gmail.com>
2020-04-17 17:03:58 +0300

    set and send response

--- express/app.js
+++ express/app.js
@@ -8,6 +8,9 @@ const app = express();
 // Listening to a port
 app.listen(port, () => console.log(`Listening to port number ${port}`));
 
-app.get('/', (req, res) => res.json(Members.jso));
-
+app.get('/', (req, res) =>  res.set('content-type', 'image/svg+xml').send(`
+    <svg width="100" height="100">
+      <circle cx="50" cy="50" r="40" stroke="blue" stroke-width="4" fill="white" />
+    </svg>
+  `));
 
