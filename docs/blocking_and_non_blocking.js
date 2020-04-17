d510704fabdc4f0174b8082cbbd4a8fdc05f4e92
Bruno Alfred <alfredybruno@gmail.com>
2020-04-17 16:45:37 +0300

    dsfcsa

--- docs/blocking_and_non_blocking.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-path.join(__dirname, 'file.md');
-fs.readFile('./file.md', (err, data) => {
-    if (err) throw err;
-    console.log(data);
-});
-
--- docs/require.js
+++
@@ -1,3 +0,0 @@
-const car = require('./modules')
-
-console.log(`${vehicle.brand}`);
