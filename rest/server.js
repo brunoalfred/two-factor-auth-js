3141e470199d08d0e59e129f1da55286d81d57d9
Bruno Alfred <alfredybruno@gmail.com>
2020-04-18 10:16:37 +0300

    complet

--- rest/server.js
+++ rest/server.js
@@ -1,6 +1,13 @@
 // Using the core node module to learn the Request / Response circle
 const http = require('http');
-const port =  process.env.port || 5000 ;
+const port = process.env.port || 5000;
 
 //TODO: Listening port on the server
+server.listen(port, () =>
+    console.log(`Server is Listening on port No. ${port}`));
 
+
+
+// ? The server is initiated
+const server = http.createServer((req, res)
+    =>  console.log(req) );
