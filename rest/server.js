81f4a3fb14f9c858a5ba82285de35d8b8a1c7d7a
Bruno Alfred <alfredybruno@gmail.com>
2020-04-18 10:11:24 +0300

    todo

--- rest/server.js
+++ rest/server.js
@@ -1,3 +1,6 @@
 // Using the core node module to learn the Request / Response circle
 const http = require('http');
 const port =  process.env.port || 5000 ;
+
+//TODO: Listening port on the server
+
