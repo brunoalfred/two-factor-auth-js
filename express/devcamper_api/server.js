347caa5f1a263dc0821dee58d24539a52873cd39
Bruno Alfred <alfredybruno@gmail.com>
2020-04-19 12:02:53 +0300

    included the config.env

--- express/devcamper_api/server.js
+++ express/devcamper_api/server.js
@@ -2,3 +2,12 @@
 const express = require('express');
 const dotenv = require('dotenv');
 
+const app = express();
+
+// LOad the config file
+dotenv.config({path: './config/config.env'});
+
+const port = process.env.PORT || 5000;
+
+app.listen(port,  () => console.log())
+
