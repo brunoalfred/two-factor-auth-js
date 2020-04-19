6753dfde65791ec1d10c440660e97e66c2b1e466
Bruno Alfred <alfredybruno@gmail.com>
2020-04-19 12:48:18 +0300

    de;ete method

--- express/devcamper_api/server.js
+++ express/devcamper_api/server.js
@@ -14,22 +14,32 @@ app.listen(port, () =>
     console.log(`server running on ${process.env.NODE_ENV} mode on port no. ${port}`));
 
 app.get('/api/v1/bootcamps', (req, res) => {
-    res.status(200).json({ success: true, msg: 'Show all the Bootcamps' })
+    res
+        .status(200)
+        .json({ success: true, msg: 'Show all the Bootcamps' })
 });
 
 app.get('/api/v1/bootcamps/:id', (req, res) => {
-    res.status(200).json({ success: true, msg: `Get  bootcamp ${req.params.id}` });
+    res
+        .status(200)
+        .json({ success: true, msg: `Get  bootcamp ${req.params.id}` });
 });
 
 app.post('/api/v1/bootcamps', (req, res) => {
-    res.status(200).json({ success: true, msg: 'Create a new Bootcamps ' })
+    res
+        .status(200)
+        .json({ success: true, msg: 'Create a new Bootcamps ' })
 });
 
 app.put('/api/v1/bootcamps/:id', (req, res) => {
-    res.status(200).json({ success: true, msg: `Update bootcamp ${req.params.id}` });
+    res
+        .status(200)
+        .json({ success: true, msg: `Update bootcamp ${req.params.id}` });
 });
 
 app.delete('/api/v1/bootcamps/:id', (req, res) => {
-    res.status(200).json({ success: true, msg: `Delete a bootcamp ${req.params.id}` })
+    res
+        .status(200)
+        .json({ success: true, msg: `Delete a bootcamp with id: ${req.params.id}` })
 });
 
