6070d383ac88a30a2b061ffb108295f78b2043c8
Bruno Alfred <alfredybruno@gmail.com>
2020-04-19 13:05:34 +0300

    used routers

--- express/devcamper_api/routes/bootcamps.js
+++ express/devcamper_api/routes/bootcamps.js
@@ -4,33 +4,34 @@ const router = express.Router();
 
 
 
-router.get('/api/v1/bootcamps', (req, res) => {
+router.get('/', (req, res) => {
     res
         .status(200)
         .json({ success: true, msg: 'Show all the Bootcamps' })
 });
 
-router.get('/api/v1/bootcamps/:id', (req, res) => {
+router.get('/:id', (req, res) => {
     res
         .status(200)
         .json({ success: true, msg: `Get  bootcamp ${req.params.id}` });
 });
 
-router.post('/api/v1/bootcamps', (req, res) => {
+router.post('/', (req, res) => {
     res
         .status(200)
         .json({ success: true, msg: 'Create a new Bootcamps ' })
 });
 
-router.put('/api/v1/bootcamps/:id', (req, res) => {
+router.put('/:id', (req, res) => {
     res
         .status(200)
         .json({ success: true, msg: `Update bootcamp ${req.params.id}` });
 });
 
-router.delete('/api/v1/bootcamps/:id', (req, res) => {
+router.delete('/:id', (req, res) => {
     res
         .status(200)
         .json({ success: true, msg: `Delete a bootcamp with id: ${req.params.id}` })
 });
 
+module.exports = router;
--- express/devcamper_api/server.js
+++ express/devcamper_api/server.js
@@ -4,10 +4,8 @@ const dotenv = require('dotenv');
 
 const app = express();
 
-// ? Include The route files
+// * Include The route files
 const bootcamps = require('./routes/bootcamps');
-
-
 
 
 // LOad the config file
@@ -18,3 +16,7 @@ const port = process.env.PORT || 5000;
 // LIstening to port number 5000 by the server
 app.listen(port, () =>
     console.log(`server running on ${process.env.NODE_ENV} mode on port no. ${port}`));
+
+
+//  Mount the routers
+app.use('/api/v1/bootcamps', bootcamps);
