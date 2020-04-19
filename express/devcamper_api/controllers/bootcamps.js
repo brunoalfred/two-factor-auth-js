afc7480cc0eae013830286cc53c22deced72eb6d
Bruno Alfred <alfredybruno@gmail.com>
2020-04-19 13:29:20 +0300

    included the controllers

--- express/devcamper_api/controllers/bootcamps.js
+++ express/devcamper_api/controllers/bootcamps.js
@@ -4,6 +4,18 @@
 
 exports.getBootcamps = (req, res, next) => {
     res
-    .status(200)
-    .json({ success: true, msg: 'Show all the Bootcamps' });
+        .status(200)
+        .json({ success: true, msg: 'Show all the Bootcamps' });
+}
+
+
+
+// @description  Get Single Bootcamps
+//  @route       GET /api/v1/bootcamps/:id
+//  @access      Public
+
+exports.getBootcamp = (req, res, next) => {
+    res
+        .status(200)
+        .json({ success: true, msg: `Get  bootcamp ${req.params.id}` });
 } 
