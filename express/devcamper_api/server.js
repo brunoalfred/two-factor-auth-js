7ed4af0343991583f4444ad2401ae545a90e7f3b
Bruno Alfred <alfredybruno@gmail.com>
2020-04-19 12:05:51 +0300

    environment runnning

--- express/devcamper_api/server.js
+++ express/devcamper_api/server.js
@@ -11,5 +11,5 @@ const port = process.env.PORT || 5000;
 
 // LIstening to port number 5000 by the server
 app.listen(port, () =>
-    console.log(`server running on port no. ${port}`));
+    console.log(`server running on ${process.env.NODE_ENV} mode on port no. ${port}`));
 
