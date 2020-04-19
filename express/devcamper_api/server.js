39ef67b8f94e435a99844284d8f8254326e075df
Bruno Alfred <alfredybruno@gmail.com>
2020-04-19 12:34:17 +0300

    modified the route

--- express/devcamper_api/server.js
+++ express/devcamper_api/server.js
@@ -13,6 +13,6 @@ const port = process.env.PORT || 5000;
 app.listen(port, () =>
     console.log(`server running on ${process.env.NODE_ENV} mode on port no. ${port}`));
 
-app.get('/', (req, res)=> {
-   res.sendStatus(400);
-})
+app.get('/api/v1/bootcamps', (req, res) => {
+    res.status(200).json({ success: true, msg: 'Show all the Bootcamps' })
+});
