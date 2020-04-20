c0a0606aee3ff95dce9da15d0233004277130008
Bruno Alfred <alfredybruno@gmail.com>
2020-04-20 13:16:17 +0300

    method to create one student

--- express/practice/student/controllers/student_profiles.js
+++ express/practice/student/controllers/student_profiles.js
@@ -1,5 +1,5 @@
 
-// @ description     Get all Student
+// @ description     Get all Student  Profile
 // @ Route           /api/vi/student_profiles
 // @ access           Public
 
@@ -9,11 +9,24 @@ exports.getStudentProfiles = (req, res, next) => {
         .json({success: true, msg: 'Show all Students'});
 }
 
+// @ description      Get a Single Student Profile
+// @ Route            /api/v1/student_profiles/1
+// @ access
+
+
 exports.getStudentProfile = (req, res, next) => {
     res
         .status(200)
         .json({success: true, msg: 'Show a Single Student'});
 
 
+}
+
+
+// @ description   Create a Student Profile
+// @ Route         /api/v1/student_profile
+// @ access        Private
+exports.createStudentProfile = (req, res, next) => {
+    res.st
 }
 
