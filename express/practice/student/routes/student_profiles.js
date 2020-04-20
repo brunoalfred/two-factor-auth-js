7033958571c97559066858888a65a82e7e20941f
Bruno Alfred <alfredybruno@gmail.com>
2020-04-20 11:17:26 +0300

    formated the code

--- express/practice/student/routes/student_profiles.js
+++ express/practice/student/routes/student_profiles.js
@@ -3,27 +3,37 @@ const router = express.Router();
 
 router.get('/', (req, res) =>{
 
-    res.status(200).json({success: true, msg: 'Show all Students'});
+    res
+        .status(200)
+        .json({success: true, msg: 'Show all Students'});
 
 
 } );
 
 router.get('/:id', (req, res) => {
 
-    res.status(200).json({success: true, msg: 'Show a Single Student'});
+    res
+        .status(200)
+        .json({success: true, msg: 'Show a Single Student'});
 
 });
 
 router.post('/', (req, res) => {
-    res.status(200).json({success: true, msg: 'Create a Student'});
+    res
+        .status(200)
+        .json({success: true, msg: 'Create a Student'});
 });
 
 router.put('/', (req, res) => {
-    res.status(200).json({success: true, msg: 'Update a Student'});
+    res
+        .status(200)
+        .json({success: true, msg: 'Update a Student'});
 });
 
 router.delete('/', (req, res) => {
-    res.status(200).json({sucess: true, msg: 'Delete a Student'});
+    res
+        .status(200)
+        .json({sucess: true, msg: 'Delete a Student'});
 })
 module.exports = router;
 
