2ac59fefe083530b2766d6993a871c545782a308
Bruno Alfred <alfredybruno@gmail.com>
2020-04-20 10:15:16 +0300

    port listening

--- express/practice/student/server.js
+++ express/practice/student/server.js
@@ -9,6 +9,12 @@ const port =  process.env.PORT || 5000;
 //importing the configuration file
 dotenv.config({path: './config/config..env'});
 
+// Server listening at port number 5000
+app.listen(port, () => {
+    console.log(`Server running on ${process.env.NODE_ENV} mode on port ${port}`);
+});
+
+
 
 
 
