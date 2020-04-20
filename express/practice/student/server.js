f31870ba0213574e811d94ba9bac9dbccb99973c
Bruno Alfred <alfredybruno@gmail.com>
2020-04-20 10:20:10 +0300

    listening port number changed to port number 5500

--- express/practice/student/server.js
+++ express/practice/student/server.js
@@ -2,14 +2,14 @@ const express = require('express');
 const dotenv = require('dotenv');
 
 const app = express();
-const port =  process.env.PORT || 5000;
+const port =  process.env.PORT || 5500;
 
 
 
 //importing the configuration file
 dotenv.config({path: './config/config.env'});
 
-// Server listening at port number 5000
+// Server listening at port number 5500
 app.listen(port, () => {
     console.log(`Server running on ${process.env.NODE_ENV} mode on port ${port}`);
 });
