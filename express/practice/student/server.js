ddba621379fc0154305a0a10a6e85360e5234510
Bruno Alfred <alfredybruno@gmail.com>
2020-04-20 09:40:08 +0300

    server js

---
+++ express/practice/student/server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const dotenv = require('dotenv');
+
+const app = express();
+const port =  process.env.PORT || 5000;
+
+
+//importing the configuration file
+dotenv.config()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
