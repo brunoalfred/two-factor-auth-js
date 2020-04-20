217c6ff0304d8f459f9e01f55ef113565eabd199
Bruno Alfred <alfredybruno@gmail.com>
2020-04-20 12:50:55 +0300

    method to get one student

--- express/practice/student/controllers/student_profiles.js
+++ express/practice/student/controllers/student_profiles.js
@@ -3,6 +3,17 @@
 // @ Route           /api/vi/student_profiles
 // @ access           Public
 
-export.getStudentProfiles = (req, res, next) => {
+exports.getStudentProfiles = (req, res, next) => {
+    res
+        .status(200)
+        .json({success: true, msg: 'Show all Students'});
+}
+
+exports.getStudentProfile = (req, res, next) => {
+    res
+        .status(200)
+        .json({success: true, msg: 'Show a Single Student'});
+
 
 }
+
