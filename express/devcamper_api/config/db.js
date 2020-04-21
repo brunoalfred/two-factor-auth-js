f6feb0f8292569ec0431742a557922bf98bc5466
Bruno Alfred <alfredybruno@gmail.com>
2020-04-21 15:21:28 +0300

    log the host

--- express/devcamper_api/config/db.js
+++ express/devcamper_api/config/db.js
@@ -2,10 +2,11 @@ const mongoose = require('mongoose');
 
 const connectDB = () => {
 
-    const connection = await mongoose.connect(process.env.MONGO_URI, {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
         useNewUrlParser: true, 
         useCreateIndex: true,
         useFindAndModify: false
     });
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
 
 }
