66776e22dba8f7150ead4f207ff567a0a700a971
Bruno Alfred <alfredybruno@gmail.com>
2020-04-21 16:38:20 +0300

    for an error occurinf

--- express/practice/student/config/db.js
+++ express/practice/student/config/db.js
@@ -1,3 +1,9 @@
 const mongoose = require('mongoose');
 
 mongoose.connect('mongodb://localhost/test', {useNewUrlParser: true});
+
+// To get notified of anything that has happened during
+// the Above process of connecting to database
+
+var db = mongoose.connection;
+db.on('error',)
