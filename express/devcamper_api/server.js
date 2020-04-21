f4eb89a9c6776b6a5aa1bcde1aec5c4e91933ed4
Bruno Alfred <alfredybruno@gmail.com>
2020-04-21 16:03:21 +0300

    global error handle

--- express/devcamper_api/server.js
+++ express/devcamper_api/server.js
@@ -32,7 +32,7 @@ const port = process.env.PORT || 5000;
 
 
 // LIstening to port number 5000 by the server
-app.listen(port, () =>
+const server =  app.listen(port, () =>
     console.log(`server running on ${process.env.NODE_ENV} mode on port no. ${port}`));
 
 
@@ -44,10 +44,12 @@ app.use('/api/v1/bootcamps', bootcamps);
 // * Development LOGGER Middleware
 // It Morgan is used to output request details on the console. 
 // Therefore, Morgan output's anything that happens on the webpage.
-
-
-
 if(process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
 }
+
+//! Handle Unhandle Promise rejection
+process.on('unhandledRejection', (err, promise) => {
+    console.log(``)
+});
 
