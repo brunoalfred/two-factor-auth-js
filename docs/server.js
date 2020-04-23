f11aa65359d6e9d1f0f4ede1d6f3b638a2f8bcbf
Bruno Alfred <alfredybruno@gmail.com>
2020-04-23 04:55:12 +0300

    npm install

--- docs/server.js
+++ docs/server.js
@@ -3,5 +3,9 @@ var app = express();
 var port = process.env.PORT ;
 
 app.get('/', (req, res)=>{
-    console.log('welcome to my servece');
+    res.send('welcome to my servece');
+});
+
+app.listen(port, () => {
+    console.log(`server running in ${NODE_ENV}in port number ${port}`); 
 });
