66095dfc7a9dc3892bf6d1ab5442b1b121f6f5b9
Bruno Alfred <alfredybruno@gmail.com>
2020-04-25 01:18:08 +0300

    created the database

--- mongodb/newspaper/repositories/person_repo.js
+++ mongodb/newspaper/repositories/person_repo.js
@@ -6,11 +6,13 @@ function person_repo() {
     const uri = 'mongodb://localhost:27017';
     const db_name = 'circulation';
 
-    
+
     function loadData(data) {
 
 
-        return new Promise((resolve))
+        return new Promise((resolve, reject) => {
+            const client = new MongoClient(uri);
+        });
 
     }
     return { loadData };
