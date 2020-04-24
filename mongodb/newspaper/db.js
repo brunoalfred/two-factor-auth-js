55c55514eaedbc0c15ee6ae1a5c0ab8ac48d3bc0
Bruno Alfred <alfredybruno@gmail.com>
2020-04-24 17:59:17 +0300

    declairing the func and calling it

--- mongodb/newspaper/db.js
+++ mongodb/newspaper/db.js
@@ -3,3 +3,10 @@ const MongoClient = require('mongodb').MongoClient;
 // declare the uri and database names
 const uri = 'mongodb://localhost:27017';
 const db_name = 'circulation';
+
+function main() {
+
+
+}
+
+main();
