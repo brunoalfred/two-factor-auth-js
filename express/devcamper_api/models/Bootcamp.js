69c01d9f6936f01c2182df752b128f454d7a9388
Bruno Alfred <alfredybruno@gmail.com>
2020-04-28 10:25:39 +0300

    checking the url string

--- express/devcamper_api/models/Bootcamp.js
+++ express/devcamper_api/models/Bootcamp.js
@@ -12,6 +12,13 @@ const BootcampSchema = new mongoose.Schema({
     description: {
         type: String,
         required: [true, 'Please add a name'],
-        maxlength: [500, 'Description cannot be more than 50 characters']
+        maxlength: [500, 'Description cannot be more than 500 characters']
+    },
+    website: {
+        type: String,
+        match: [
+            /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/,
+        'Please use a valid URL with HTTP or HTTPS'
+        ]
     }
 });
