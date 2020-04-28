b67b45dc1b0b7a99f9f94e4b0cbba1a81edd63ef
Bruno Alfred <alfredybruno@gmail.com>
2020-04-28 10:54:43 +0300

    finished the location field

--- express/devcamper_api/models/Bootcamp.js
+++ express/devcamper_api/models/Bootcamp.js
@@ -39,5 +39,23 @@ const BootcampSchema = new mongoose.Schema({
      },
      location: {
         //  GeoJson  
-     }
+        type: {
+            type: String,
+            enum: ['Point'],
+            required: true
+        },
+        coordinates: {
+            type: [Number],
+            required: true,
+            index: '2dsphere'
+        },
+        formattedAddress: String,
+        street: String,
+        city: String,
+        state: String,
+        zipcode: String,
+        country: String,
+     },
+     
+
 });
