96211855bb07c094ef524ba8e906dcea521f7006
Bruno Alfred <alfredybruno@gmail.com>
2020-04-28 10:57:01 +0300

    the carreer field is required and the options are given

--- express/devcamper_api/models/Bootcamp.js
+++ express/devcamper_api/models/Bootcamp.js
@@ -56,6 +56,19 @@ const BootcampSchema = new mongoose.Schema({
         zipcode: String,
         country: String,
      },
-     
+
+     careers: {
+        //  Array of strings
+        type: [String],
+        required: true,
+        enum: [
+            'Web Development',
+            'Mobile Development',
+            'UI/UX',
+            'Data Science',
+            'Business',
+            'Other'
+        ]
+     }
 
 });
