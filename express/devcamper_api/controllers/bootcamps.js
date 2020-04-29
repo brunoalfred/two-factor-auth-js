88cceb524c27f262f3dc3e58df8c60e81ce8ed3c
Bruno Alfred <alfredybruno@gmail.com>
2020-04-29 14:50:49 +0300

    feature:  made the controller to insert new data to the database

--- express/devcamper_api/controllers/bootcamps.js
+++ express/devcamper_api/controllers/bootcamps.js
@@ -32,11 +32,14 @@ exports.getBootcamp = (req, res, next) => {
 //  @route         POST /api/v1/bootcamps
 //  @access        Private
 
-exports.createBootcamp = (req, res, next) => {
-    console.log(req.body)
-    res
-        .status(200)
-        .json({ success: true, msg: 'Create a new Bootcamps ' });
+exports.createBootcamp = async (req, res, next) => {
+
+    // ? creating a new value into the collection
+    const bootcamp = await Bootcamp.create(req.body);
+
+    res.status(201)
+    .json({succes: true, data: {bootcamp}});
+
 }
 
 
