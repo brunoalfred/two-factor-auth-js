7fc56d5383b09a32b6782091ae3f2a6178f6adc0
Bruno Alfred <alfredybruno@gmail.com>
2020-04-30 13:17:56 +0300

    fix(db config): Fixed the error due to improper uri passing in the connect method for mongoose.

--- express/practice/student/config/db.js
+++ express/practice/student/config/db.js
@@ -1,17 +1,20 @@
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config({path: './config/config.env'});
 
 //method to connect to the database
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect('process.env.MONGO_URI',
+        const conn = await mongoose.connect('mongodb://localhost:27017/student',
             {
                 useNewUrlParser: true,
                 useCreateIndex: true,
                 useFindAndModify: true,
-                useUnifiedTopology: true
+                useUnifiedTopology: true,
             }
         );
-        console.log(`MongoDB connected: ${conn.connection.host}`);
+        console.log(`MongoDB connected: ${conn.connection.host}:${conn.connection.port}`);
     } catch (err) {
         console.log(err);
     }
