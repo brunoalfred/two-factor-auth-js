795c937480d1baec2ae24476742c8faecc0f280c
Bruno Alfred <alfredybruno@gmail.com>
2020-04-30 15:34:56 +0300

    feat(controller): implemented the creating method  for POST http VERB.

--- express/practice/student/controllers/student_profiles.js
+++ express/practice/student/controllers/student_profiles.js
@@ -7,7 +7,6 @@ const Student_profiles = require('../models/Student_profiles');
 // @ access           Public
 
 exports.getStudentProfiles = (req, res, next) => {
-    console.log(req.body);
     res
         .status(200)
         .json({success: true, msg: 'Show all Students'});
@@ -32,8 +31,12 @@ exports.getStudentProfile = (req, res, next) => {
 // @ access        Private
 exports.createStudentProfile = async (req, res, next) => {
     try {
-        const student_profile = await
+        const student_profile = await Student_profiles.create(req.body);
+        res.status(201)
+            .json({success: true, data: student_profile});
     } catch (err) {
+        res.status(400)
+            .json({success: false});
         console.log(err);
     }
 }
