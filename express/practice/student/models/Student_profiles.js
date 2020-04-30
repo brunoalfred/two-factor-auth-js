afc00f10e766194cb47f859bfb788fc835d1a6ff
Bruno Alfred <alfredybruno@gmail.com>
2020-04-30 09:01:33 +0300

    feat: created the name field for the studentProfile schema.

--- express/practice/student/models/Student_profiles.js
+++ express/practice/student/models/Student_profiles.js
@@ -1,2 +1,12 @@
 const mongoose = require('mongoose');
-const Student_profilesSchema = new mongoose.Schema({});
+const Student_profilesSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        require: [true, 'Please add the name'],
+        unique: true,
+        trim: true,
+        maxlength: [50, 'Name cannot be more than 50 characters']
+
+    }
+
+});
