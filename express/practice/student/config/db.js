8e601fcfda7cb2f1c8bc4acc668070175d0290d0
Bruno Alfred <alfredybruno@gmail.com>
2020-04-30 09:24:04 +0300

    feat: connecting to MongoDB database in localhost:27017

--- express/practice/student/config/db.js
+++ express/practice/student/config/db.js
@@ -1,4 +1,13 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/test', {useNewUrlParser: true});
-
+//method to connect to the database
+const connectDB = async () => {
+    try {
+        const conn = await mongoose.connect(process.env.MONGO_URI,
+            {useNewUrlParser: true,}
+        );
+        console.log(`MongoDB connected: ${conn.connection.host}`);
+    } catch (err) {
+        console.log(err);
+    }
+}
