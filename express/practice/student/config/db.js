4028b98e5476bcf281f5b0ac72a641a7aa4526de
Bruno Alfred <alfredybruno@gmail.com>
2020-04-30 09:14:12 +0300

    fix: deleted the statements which could not produce the desired.

--- express/practice/student/config/db.js
+++ express/practice/student/config/db.js
@@ -2,8 +2,3 @@ const mongoose = require('mongoose');
 
 mongoose.connect('mongodb://localhost/test', {useNewUrlParser: true});
 
-// To get notified of anything that has happened during
-// the Above process of connecting to database
-
-var db = mongoose.connection;
-db.on('error', console.er)
