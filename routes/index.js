f659678e1944a47238e53c1e5383573274ddb6d8
bruno alfred <alfredybruno@gmail.com>
2020-11-24 13:11:30 +0300

    Sendind a plain Html

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,9 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   // res.render('index', { title: 'Express' });
-  res.sendFile(path.join(__dirname + '/index.html'));
+  res.sendFile('index.html', {
+    root: 'views'
+  });
 });
 
 module.exports = router;
