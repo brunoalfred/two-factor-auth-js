14db4657a8a303ee2f62d5c127b5887f7264cb54
bruno alfred <alfredybruno@gmail.com>
2020-11-26 19:07:03 +0300

    Create verify.js

---
+++ routes/verify.js
@@ -0,0 +1,44 @@
+var express = require("express");
+const speakeasy = require("speakeasy");
+const JsonDB = require("node-json-db").JsonDB;
+const Config = require("node-json-db/dist/lib/JsonDBConfig").Config;
+
+
+var router = express.Router();
+
+
+var db = new JsonDB(new Config("myDataBase", true, false, '/'));
+
+
+
+/* GET home page. */
+router.post("/", function (req, res, next) {
+  const { userId, token } = req.body;
+  try {
+    // Retrieve user from the Database
+    const path = `/user/${userId}`;
+    const user = db.getData(path);
+    console.log({ user });
+    const { base32: secret } = user.temp_secret;
+    const verified = speakeasy.totp.verify({
+      secret,
+      encoding: "base32",
+      token,
+    });
+
+    if (verified) {
+      // Update user data
+      db.push(path, { id: userId, secret: user.temp_secret });
+      res.json({ verified: true });
+    } else {
+      res.json({ verified: false });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Error retrieving user" });
+  }
+
+  // res.render('login', { title: 'Express' });
+});
+
+module.exports = router;
