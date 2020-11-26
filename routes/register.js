1ea7aedae921433eca9f36650a0fcd6536acdc2d
bruno alfred <alfredybruno@gmail.com>
2020-11-26 19:06:16 +0300

    Feat: Registering User

--- routes/register.js
+++ routes/register.js
@@ -1,10 +1,27 @@
-var express = require('express');
+var express = require("express");
+const speakeasy = require("speakeasy");
+const JsonDB = require("node-json-db").JsonDB;
+const Config = require("node-json-db/dist/lib/JsonDBConfig").Config;
+const uuid = require("uuid");
+
+var db = new JsonDB(new Config("myDataBase", true, false, '/'));
+
+
+
 var router = express.Router();
 
 /* GET home page. */
-router.get('/register', function(req, res, next) {
-
-
+router.post("/", function (req, res, next) {
+  const id = uuid.v4();
+  try {
+    const path = `/user/${id}`;
+    const temp_secret = speakeasy.generateSecret();
+    db.push(path, { id, temp_secret });
+    res.json({ id, secret: temp_secret.base32 });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Error generating secret key" });
+  }
 
   // res.render('register', { title: 'Express' });
 });
