7f5e8128c2df38fddcc20c863b1e788949651144
bruno alfred <alfredybruno@gmail.com>
2020-12-01 18:21:19 +0300

    Fix:

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
+
+
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
